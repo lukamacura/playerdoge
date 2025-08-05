b1d6e78b6c9464f9752c024b443e334fda3b858a
Luka Macura <designmacura@gmail.com>
2025-08-05 09:08:50 +0200

    Blog ispravka

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,15 +40,16 @@ export default function Footer() {
                 href: "mailto:support@playerdoge.com",
               },
               {
-                icon: "/icons/instagram.png",
-                label: "@packloader",
-                href: "https://www.instagram.com/packloader/",
-              },
-              {
                 icon: "/icons/discord.png",
                 label: "PlayerDoge",
                 href: "https://discord.gg/ntgxjAhtUQ",
               },
+              {
+                icon: "/icons/instagram.png",
+                label: "@packloader",
+                href: "https://www.instagram.com/packloader/",
+              },
+              
             ].map((contact) => (
               <Link
                 key={contact.label}
